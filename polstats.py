39ca48f9adb607575c6be52491083f1d387ab6b5
nicky-eng <nicolasoyarzabal@gmail.com>
2021-03-11 17:46:56 +0100

    Setting pretty before Article End Point.

--- polstats.py
+++ polstats.py
@@ -1,9 +1,17 @@
-from logging import DEBUG
 from flask import Flask, render_template
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
@@ -14,7 +22,7 @@ def index():
 def user(name):
     lazy_users = ['ignacio', 'xavier', 'pablo', 'matias', 'matías', 'gabriel']
     if name.lower() in lazy_users:
-        return render_template('lazy_user.html', name=name.title())
+        return render_template('lazy_user.html', name=name)
     else:
-        return render_template('user.html', name=name.title())
+        return render_template('user.html', name=name)
 
