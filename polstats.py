1f90ea7db499ac5f198f5166306f6cf135d5a67d
nicky-eng <nicolasoyarzabal@gmail.com>
2021-03-14 19:43:12 +0100

    Article endpoint working but unfinished.

--- polstats.py
+++ polstats.py
@@ -1,49 +1,53 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+
+from flask import Flask, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, DateField
-from wtforms.validators import DataRequired, URL
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import DateField, IntegerField, StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'hard to guess secret key'
-app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "data.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "hard to guess secret key"
+app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
 
-@app.route('/')
+@app.route("/")
 def index():
     form = ArticleForm()
-    return render_template('index.html', form=form)
+    return render_template("index.html", form=form)
 
 
-@app.route('/user/<name>')
+@app.route("/user/<name>")
 def user(name):
-    lazy_users = ['ignacio', 'xavier', 'pablo', 'matias', 'matías', 'gabriel']
+    lazy_users = ["ignacio", "xavier", "pablo", "matias", "matías", "gabriel"]
     if name.lower() in lazy_users:
-        return render_template('lazy_user.html', name=name)
+        return render_template("lazy_user.html", name=name)
     else:
-        return render_template('user.html', name=name)
+        return render_template("user.html", name=name)
 
 
 class Article(db.Model):
-    __tablename__ = 'countries'
+    __tablename__ = "countries"
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     url = db.Column(db.String)
     rating = db.Column(db.Integer)
@@ -56,25 +60,32 @@ class Article(db.Model):
     def __repr__(self):
         return f"{self.title} publicado en  {self.source}"
 
+
 class ArticleForm(FlaskForm):
-    title = StringField('News article title: ', validators=[DataRequired()])
-    rating = IntegerField('News sentiment rating: ', validators=[DataRequired()])
-    submit =SubmitField('Submit')
-    
-@app.route('/articles', methods=['GET'])
+    title = StringField("News article title: ", validators=[DataRequired()])
+    rating = IntegerField("News sentiment rating: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/articles", methods=["GET"])
 def articles():
     articles = Article.query.all()
-    return render_template('articles.html', articles=articles)
+    return render_template("articles.html", articles=articles)
 
 
-@app.route('/addarticle', methods=['GET', 'POST'])
-def addarticle():
+@app.route("/add_article", methods=["GET"])
+def add_article_get():
+    form_add_article = ArticleForm()
+    return render_template("add_article.html", form=form_add_article)
+
+
+@app.route("/add_article", methods=["POST"])
+def add_article_post():
     form = ArticleForm()
     if form.validate_on_submit():
-        db.session.add(ArticleForm.title)
-        db.session.add(ArticleForm.rating)
+        title = form.title.data
+        rating = form.rating.data
+        new_article = Article(title=title, rating=rating)
+        db.session.add(new_article)
         db.session.commit()
-        return redirect(url_for('addarticle'))
-    return render_template('addarticle.html', form=form)
-
-
+        return redirect(url_for("add_article_get"))
