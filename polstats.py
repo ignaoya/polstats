80ed98205e26684a05c05382777d441ffaa42a90
Nicolas A. Oyarzabal <nicolasoyarzabal@gmail.com>
2021-03-17 10:57:49 +0100

    Article End-Point finished.

--- polstats.py
+++ polstats.py
@@ -1,19 +1,19 @@
 import os
+import datetime 
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import DateField, IntegerField, StringField, SubmitField
-from wtforms.validators import URL, DataRequired
+from wtforms.validators import URL, DataRequired, Optional
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    basedir, "data.sqlite"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "hard to guess secret key"
 app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 
@@ -47,11 +47,11 @@ def user(name):
 
 
 class Article(db.Model):
-    __tablename__ = "countries"
+    __tablename__ = "articles"
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     url = db.Column(db.String)
     rating = db.Column(db.Integer)
-    date = db.Column(db.Date)
+    date = db.Column(db.Date, default=datetime.date.today())
     length = db.Column(db.Integer)
     source = db.Column(db.String)
     countries = db.Column(db.String)
@@ -64,20 +64,18 @@ class Article(db.Model):
 class ArticleForm(FlaskForm):
     title = StringField("News article title: ", validators=[DataRequired()])
     rating = IntegerField("News sentiment rating: ", validators=[DataRequired()])
+    source = StringField("News channel: ", validators=[DataRequired()])
+    url = StringField("News web link: ", validators=[URL()])
+    date = DateField("Date of the news: ", validators=[Optional()])
+    length = IntegerField("Length of the news article in words: ")
+    countries = StringField("Countries involved: ")
     submit = SubmitField("Submit")
 
 
-@app.route("/articles", methods=["GET"])
-def articles():
-    articles = Article.query.all()
-    return render_template("articles.html", articles=articles)
-
-
-@app.route("/add_article", methods=["GET"])
+@app.route("/add_article", methods=['GET'])
 def add_article_get():
-    form_add_article = ArticleForm()
-    return render_template("add_article.html", form=form_add_article)
-
+    form = ArticleForm()
+    return render_template("add_article.html", form=form)
 
 @app.route("/add_article", methods=["POST"])
 def add_article_post():
@@ -85,7 +83,18 @@ def add_article_post():
     if form.validate_on_submit():
         title = form.title.data
         rating = form.rating.data
-        new_article = Article(title=title, rating=rating)
+        source = form.source.data
+        url = form.url.data
+        date = form.date.data
+        length = form.length.data
+        countries = form.countries.data
+        new_article = Article(title=title, rating=rating, source=source, url=url, date=date, length=length, countries=countries)
         db.session.add(new_article)
         db.session.commit()
+        flash('Article added to the data base. Thank you!')
         return redirect(url_for("add_article_get"))
+
+@app.route("/view_articles", methods=["GET"])
+def view_articles():
+    articles = Article.query.all()
+    return render_template("view_articles.html", articles=articles)
